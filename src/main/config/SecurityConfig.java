c96b82231de890540e550714a0db2d13d537f00a
wang1985 <m0804105@im.tpcu.edu.tw>
2023-02-21 17:22:15 +0800

    230221_1721

---
+++ src/main/config/SecurityConfig.java
@@ -0,0 +1,63 @@
+package main.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
+	
+	@Bean
+	public AccessDeniedHandler accessDeniedHandler() {
+		return new EshopAccessDeniedHandler();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+			.inMemoryAuthentication()
+			.withUser("John").password(passwordEncoder().encode("admin")).roles("ADMIN", "EMPLOYEE")
+			.and()
+			.withUser("Eric").password(passwordEncoder().encode("employee")).roles("EMPLOYEE")
+			.and()
+			.withUser("Michael").password(passwordEncoder().encode("client")).roles("CLIENT");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/", "/login")
+				.permitAll()
+			.antMatchers("/addTour")
+				.hasAnyRole("ADMIN", "EMPLOYEE")
+			.and()
+				.formLogin()
+				.loginPage("/login")
+				.loginProcessingUrl("/checkUserAccount")
+				.defaultSuccessUrl("/")
+				.permitAll()
+			.and()
+				.logout()
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+				.logoutSuccessUrl("/")
+				.invalidateHttpSession(true)
+				.permitAll()
+			.and()
+				//.exceptionHandling().accessDeniedPage("/forbidden");
+				.exceptionHandling().accessDeniedHandler(accessDeniedHandler());
+	}
+	
+}
