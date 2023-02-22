82e9c259b64e3c96671c2f5df92b5d7253f3195c
wang1985 <m0804105@im.tpcu.edu.tw>
2023-02-22 16:11:43 +0800

    230222_1611

--- src/main/config/WebConfig.java
+++ src/main/config/WebConfig.java
@@ -12,6 +12,7 @@ import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
@@ -42,7 +43,8 @@ public class WebConfig implements WebMvcConfigurer{
 
 	@Bean
 	public SpringTemplateEngine templateEngine() {
-		SpringTemplateEngine templateEngine = new SpringTemplateEngine(); 
+		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+		templateEngine.addDialect(new SpringSecurityDialect());
 		templateEngine.setTemplateResolver(templateResolver());
 		return templateEngine;
 	}
